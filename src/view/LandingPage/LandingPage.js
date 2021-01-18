0ad5e270154adfc03555c15ffbcf7439cf2b0a80
blaze <blazejos1230@gmail.com>
2021-01-19 00:29:58 +0100

    Created IconsSection

--- src/view/LandingPage/LandingPage.js
+++ src/view/LandingPage/LandingPage.js
@@ -1,15 +1,19 @@
+import React from "react";
 import './LandingPage.css';
 import {Layout} from "antd";
 import HeheSection from "../../container/HeheSection/HeheSection";
+import IconsSection from "../../container/IconsSection/IconsSection";
+
 
 const { Content } = Layout;
 
 function LandingPage() {
   return (
       <Layout>
-          <Content>
-              <HeheSection />
-          </Content>
+        <Content>
+          {/*<HeheSection />*/}
+          <IconsSection/>
+        </Content>
       </Layout>
   );
 }
