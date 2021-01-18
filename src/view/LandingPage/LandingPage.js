ea82509161a31ecc10c8c8aeb0a11c615441abda
blaze <blazejos1230@gmail.com>
2021-01-19 00:36:41 +0100

    Created MapView

--- src/view/LandingPage/LandingPage.js
+++ src/view/LandingPage/LandingPage.js
@@ -3,19 +3,21 @@ import './LandingPage.css';
 import {Layout} from "antd";
 import HeheSection from "../../container/HeheSection/HeheSection";
 import IconsSection from "../../container/IconsSection/IconsSection";
+import MapView from "../../container/MapView/MapView";
 
 
 const { Content } = Layout;
 
 function LandingPage() {
-  return (
-      <Layout>
-        <Content>
-          {/*<HeheSection />*/}
-          <IconsSection/>
-        </Content>
-      </Layout>
-  );
+    return (
+        <Layout>
+            <Content>
+                {/*<HeheSection />*/}
+                <MapView/>
+                <IconsSection/>
+            </Content>
+        </Layout>
+    );
 }
 
 export default LandingPage;
