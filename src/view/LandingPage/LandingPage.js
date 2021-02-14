71debe2955fea2264b1a43ada1ab027a5a3e06a7
blaze <blazejos1230@gmail.com>
2021-02-14 15:01:37 +0100

    Setup

--- src/view/LandingPage/LandingPage.js
+++ src/view/LandingPage/LandingPage.js
@@ -1,23 +1,27 @@
 import React from "react";
 import './LandingPage.css';
 import {Layout} from "antd";
-import HeheSection from "../../container/HeheSection/HeheSection";
+import PartnersSection from '../../container/PartnersSection/PartnersSection';
+import ContactSection from "../../container/ContactSection/ContactSection";
 import IconsSection from "../../container/IconsSection/IconsSection";
-import MapView from "../../container/MapSection/MapView";
+import MapView from "../../container/MapView/MapView";
+import AboutUsSection from "../../container/AboutUsSection/AboutUsSection";
 
 
 const { Content } = Layout;
 
 function LandingPage() {
-  return (
-      <Layout>
-          <Content>
-              {/*<HeheSection />*/}
-              <MapView/>
-              <IconsSection/>
-          </Content>
-      </Layout>
-  );
+    return (
+        <Layout>
+            <Content>        
+                <MapView/>
+                <IconsSection/>
+                <AboutUsSection />
+                <PartnersSection />
+                <ContactSection/>
+            </Content>
+        </Layout>
+    );
 }
 
 export default LandingPage;
